c1ea76269ccb55780684a9593353f77f0a412883
tieppbrovv <thob0k22ltpun2suy@hotmail.com>
2024-05-07 13:17:01 +0700

    feat

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function permute(nums) {
-  const result = [];
-  backtrack([]);
-  return result;
-  function backtrack(current) {
-    if (current.length === nums.length) {
-      result.push([...current]);
-      return;
-    }
-    for (const num of nums) {
-      if (current.includes(num)) continue;
-      current.push(num);
-      backtrack(current);
-      current.pop();
-    }
-  }
+function rotateArray(arr, n) {
+  return arr.slice(n).concat(arr.slice(0, n));
 }
