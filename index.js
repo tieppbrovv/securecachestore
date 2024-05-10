335d3731140a77bd360ed5659e1d2e08bcdba1f7
tieppbrovv <thob0k22ltpun2suy@hotmail.com>
2024-05-10 15:15:50 +0700

    start

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
-  let hasCycle = false;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      hasCycle = true;
-      break;
-    }
+function longestPalindrome(s) {
+  let longest = "";
+  for (let i = 0; i < s.length; i++) {
+    const oddPalindrome = expandAroundCenter(s, i, i);
+    const evenPalindrome = expandAroundCenter(s, i, i + 1);
+    const currentLongest =
+      oddPalindrome.length > evenPalindrome.length
+        ? oddPalindrome
+        : evenPalindrome;
+    if (currentLongest.length > longest.length) longest = currentLongest;
   }
-  if (!hasCycle) return null;
-  slow = head;
-  while (slow !== fast) {
-    slow = slow.next;
-    fast = fast.next;
+  return longest;
+}
+function expandAroundCenter(s, left, right) {
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    left--;
+    right++;
   }
-  return slow;
+  return s.slice(left + 1, right);
 }
