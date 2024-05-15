f2682afee3a707b3266274b4e6c2e73ec523f4eb
tieppbrovv <thob0k22ltpun2suy@hotmail.com>
2024-05-15 21:24:04 +0700

    update project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
+  }
+  return prefix;
+}
